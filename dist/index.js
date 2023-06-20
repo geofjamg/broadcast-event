621c48c9781df6c06a6bd01b47d1645464c8ab17
LE SAULNIER Kevin <kevin.lesaulnier@rte-france.com>
2023-06-20 17:37:03 +0200

    fix: filter was applied wrongly
    Signed-off-by: LE SAULNIER Kevin <kevin.lesaulnier@rte-france.com>

--- dist/index.js
+++ dist/index.js
@@ -6420,60 +6420,61 @@ const core = __nccwpck_require__(89);
 const { request } = __nccwpck_require__(1133);
 
 async function run() {
-    try {
-        const token = core.getInput("token");
-        const eventType = core.getInput("event-type");
+  try {
+    const token = core.getInput("token");
+    const eventType = core.getInput("event-type");
 
-        core.debug(`Event type: ${eventType}`);
+    core.debug(`Event type: ${eventType}`);
 
-        // get owner of the current repository
-        const owner = process.env.GITHUB_REPOSITORY.split('/')[0];
-        core.debug(`Owner: ${owner}`);
+    // get owner of the current repository
+    const owner = process.env.GITHUB_REPOSITORY.split("/")[0];
+    core.debug(`Owner: ${owner}`);
 
-        const additionalOrganizations = core.getInput("organizations");
-        core.debug(`Organizations: ${additionalOrganizations}`);
+    const additionalOrganizations = core.getInput("organizations");
+    core.debug(`Organizations: ${additionalOrganizations}`);
 
-        const organizations = additionalOrganizations ? [owner, ...additionalOrganizations.split(' ')] : [owner];
-        core.info(`Organizations: ${organizations}`);
+    const organizations = additionalOrganizations
+      ? [owner, ...additionalOrganizations.split(" ")]
+      : [owner];
+    core.info(`Organizations: ${organizations}`);
 
-        for (const index in organizations) {
-            const organization = organizations[index];
+    for (const index in organizations) {
+      const organization = organizations[index];
 
-            // get repository list for this organization
-            const result = await request(`GET /users/${organization}/repos{?per_page}`,
-                {
-                    per_page: "100"
-                });
-
-            // send the event to all repositories which are not archived (otherwise the post call throws a 404 error)
-            const unarchivedRepositories = result.filter(
-                (repositories) => !repositories.archived
-            );
-
-            const unarchivedRepositoriesNames = unarchivedRepositories.data.map((repo) => repo.name);
-
-            await Promise.all(
-                unarchivedRepositoriesNames.map(
-                    (repository) => {
-                        core.info(
-                            `Send event to repository: ${organization}/${repository}`
-                        );
-                        return request(`POST /repos/${organization}/${repository}/dispatches`, {
-                            headers: {
-                                authorization: `token ${token}`,
-                            },
-                            mediaType: {
-                                previews: ["everest"],
-                            },
-                            event_type: `${eventType}`,
-                        })
-                    }
-                )
-            );
+      // get repository list for this organization
+      const result = await request(
+        `GET /users/${organization}/repos{?per_page}`,
+        {
+          per_page: "100",
         }
-    } catch (error) {
-        core.setFailed(error.message);
+      );
+
+      // send the event to all repositories which are not archived (otherwise the post call throws a 404 error)
+      const unarchivedRepositories = result.data.filter(
+        (repositories) => !repositories.archived
+      ).map((repo) => repo.name);;
+
+      await Promise.all(
+        unarchivedRepositories.map((repository) => {
+          core.info(`Send event to repository: ${organization}/${repository}`);
+          return request(
+            `POST /repos/${organization}/${repository}/dispatches`,
+            {
+              headers: {
+                authorization: `token ${token}`,
+              },
+              mediaType: {
+                previews: ["everest"],
+              },
+              event_type: `${eventType}`,
+            }
+          );
+        })
+      );
     }
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
 
 run();
